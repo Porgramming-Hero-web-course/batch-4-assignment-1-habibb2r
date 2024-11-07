c772d6d58a51dc4daef178b534bba0ac9032adf4
habibb2r <habibhk127@gmail.com>
2024-11-07 23:40:00 +0600

    Solved Problem 3

--- Problem-1.ts
+++ Problem-1.ts
@@ -2,15 +2,16 @@
 
     type ArrayList = number[];
     
-    const sumArray =  (lists : ArrayList) =>{
-        let initialNumber : number = 0;
+    const sumArray =  (lists : ArrayList): number =>{
+        let sum : number = 0;
         lists.map((num : number) =>{
-            initialNumber += num
+            sum += num
         })
-        console.log(initialNumber)
+        return sum
     }
 
     sumArray([1, 2, 3, 4, 5])
+    console.log(sumArray([1, 2, 3, 4, 5]))
 
 
 }
--- Problem-2.ts
+++ Problem-2.ts
@@ -1,10 +1,11 @@
 {
 
     type ArrayList = number[];
-    const removeDuplicates = (list: ArrayList) =>{
+    const removeDuplicates = (list: ArrayList): ArrayList =>{
         const uniqueList: number[] = [...new Set(list)];
-        console.log(uniqueList);
+        return uniqueList;
     }
 
     removeDuplicates([1, 2, 3, 4, 2, 3, 5, 6, 7, 8, 9, 10]);
+    console.log(removeDuplicates([1, 2, 3, 4, 2, 3, 5, 6, 7, 8, 9, 10]))
 }
